90eb01c832e03fd77ef46946c8ba47f2e5fc0e52
telv1n <telv1n@users.noreply.github.com>
2016-12-23 12:25:40 -0700

    Add success callback, add debug.log to gitignore

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,6 +5,12 @@ $(function() {
     url: 'https://www.codeschool.com/users/2514445.json',
     dataType: 'jsonp',
     success: function(response) {
+      var courseCompleted = response.courses.completed;
+      var courseItem = $.map(courseCompleted, function(course, i) {
+        var elements = $('<div class="course"></div>');
+        return elements;
+      });
+      $('#badge').html(courseItem);
 
     }
   });
